839e723689c812f468dd4aa36fb480c588757977
wwconslt <141940456+wwconslt@users.noreply.github.com>
2026-02-01 09:48:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = (
         .select(col("FRUIT_NAME"), col("SEARCH_ON"))
 )
 
-# Convert Snowpark DataFrame to Pandas DataFrame
+# Convert Snowpark DataFrame to Pandas DataFrame so we can use loc/iloc
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
@@ -37,5 +37,27 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
-        # Look up the API search value from the SEARCH_ON column
-        search_on = pd_
+        # Get the SEARCH_ON value for the selected FRUIT_NAME
+        search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"].iloc[0]
+
+        st.subheader(fruit_chosen + " Nutrition Information")
+
+        smoothiefroot_response = requests.get(
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+        )
+
+        st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button("Submit Order")
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
