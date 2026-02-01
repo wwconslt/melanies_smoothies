f4e8bc0d432ffb2ec6758ef990d2e29d4c1bab19
wwconslt <141940456+wwconslt@users.noreply.github.com>
2026-02-01 09:28:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,34 @@ fruit_list = [r["FRUIT_NAME"] for r in rows if r["FRUIT_NAME"] is not None]
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
+    fruit_list,
+    max_selections=5
+)
+
+if ingredients_list:
+    ingredients_string = ""
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
+
+        st.subheader(fruit_chosen + " Nutrition Information")
+
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+        )
+
+        st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button("Submit Order")
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
